098279732f8acc7411c7a08259ced5af398db7e2
anvian <andre.carbajal.vargas@gmail.com>
2022-07-19 23:27:30 -0500

    main

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-//hace un hover a la foto de perfi
-var img = document.getElementById('foto');
-img.addEventListener('mouseenter', function(){
-    this.src = "../img/otro_perfil.jpg";
-}, false);
-img.addEventListener('mouseleave', function(){
-    this.src = "../img/foto_de_perfil.jpg"
-}, false);
+//show discord id
+var discord = document.getElementById('discordID');
+discord.addEventListener("click", doClick);
+
+function doClick(){
+    discord.textContent = "pinhacolada.2#5607";
+    navigator.clipboard.writeText('pinhacolada.2#5607');
+    alert("Copied to clipboard");
+}
